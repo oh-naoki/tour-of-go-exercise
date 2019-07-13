c3ec100ff1c927e6b379ddd6cc13ba0242ad2f6b
oh-naoki <ohkibi.nao@gmail.com>
2019-07-14 01:16:33 +0900

    add errors

---
+++ errors.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	
+	z := float64(2.)
+	s := float64(0)
+	for i := 0; i < 10; i++ {
+		z = z - (z*z-x)/(2*z)
+		if math.Abs(z-s) < 1e-10 {
+			break
+		}
+		s = z
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
+
